8342516f4faa8d82b8a2d67ea4311eafb9afc4e7
slehman133 <43526258+slehman133@users.noreply.github.com>
2023-12-10 13:55:40 -0800

    boss moves babyyyyyyyyyyyy

---
+++ Assets/Scripts/Enemy/CommanderBossMoves.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+
+public class CommanderBossMoves : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+
+
+    private float timer;
+    private float bulletTimer;
+    private GameObject player;
+    private Vector3 playerCurPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        bulletTimer += Time.deltaTime;
+        playerCurPos = player.transform.position;
+        TeleportMove();
+        Shoot();
+    }
+
+    void TeleportMove()
+    {
+        int randomNumber = Random.Range(0, 2);
+        if(timer > 10)
+        {
+            if(randomNumber == 0){
+                transform.position = new Vector3(playerCurPos.x + 3f, playerCurPos.y, playerCurPos.z);
+            }else{
+                transform.position = new Vector3(playerCurPos.x - 3f, playerCurPos.y, playerCurPos.z);
+            }
+
+            timer = 0;
+        }
+    }
+      
+    void Shoot(){
+        if(bulletTimer > 2)
+            {
+                bulletTimer = 0;
+                Vector3 top = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
+                Vector3 bot = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
+                Instantiate(bullet, top, Quaternion.identity);
+                Instantiate(bullet, transform.position, Quaternion.identity);
+                Instantiate(bullet, bot, Quaternion.identity);
+
+            }
+    }
+}
+    
