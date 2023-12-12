f04d1b8b972e331fd8c081d7d7bfc07fe2d784db
slehman133 <43526258+slehman133@users.noreply.github.com>
2023-12-11 19:47:07 -0800

    pistol and minigun

---
+++ Assets/Scripts/Weapons/Weapon Controllers/MinigunController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigunController : WeaponController
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Attack()
+    {
+        base.Attack();
+        GameObject spawnedBullet = Instantiate(weaponData.Prefab);
+        spawnedBullet.transform.position = transform.position;
+        spawnedBullet.GetComponent<PistolBehavior>().DirectionChecker(pm.lastMovedDirVector);
+    }
+}
